aae8ec4b992c84732abaf6b85f8b2c4c8b05fa20
unknown <itsmeabhi>
2017-08-12 00:54:12 +0530

    App

---
+++ Registration/app/src/main/java/com/example/hp/registration/connectionurls.java
@@ -0,0 +1,13 @@
+package com.example.hp.registration;
+
+/**
+ * Created by hp on 6/11/2017.
+ */
+public class connectionurls {
+
+    public static String URL_LOGIN ="http://192.168.43.150/andro/endregister.php";
+
+    public static String URL_REGISTER="http://192.168.43.150/andro/register.php";
+
+
+}
